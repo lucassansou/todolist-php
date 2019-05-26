1e1b756e27e7dd326a4885f5866cda3017e8036c
Mayara Ramos <maya.r.rebelo@gmail.com>
2019-05-25 23:49:15 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+  
+    <meta charset="UTF-8">
+    <meta name="viewport" width=device-width initial-scale=1.0>
+    <link rel="stylesheet" type="text/css" href="main.css">
+
+    <title>To Do List</title>
+ 
+  </head>
+
+  <body>
+
+    <div class="container">
+
+      <h1>To Do List</h1>
+
+      <ul>
+        <input type="checkbox" id="pao" name="pao">
+        <label for="pao">Comprar Pão</label>
+      </ul>
+
+      <div class="input-button">
+        <input class="new-task-input" type="text" placeholder="New Task" autofocus>
+
+        <button>Add</button>
+
+      </div>
+
+    </div>
+
+  </body>
+
+</html>
